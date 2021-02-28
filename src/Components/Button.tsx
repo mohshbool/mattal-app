233bbb51de79b2f7450e3cb5e9d95b80eb8a5be0
Mohammad Shbool <alshboolmoh@gmail.com>
2021-02-28 23:42:26 +0200

    <Button />

---
+++ src/Components/Button.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Fonts from '../Theme/Fonts';
+import {Colors} from '../Theme/Theme';
+
+interface ButtonProps {
+  text: string;
+  onPress: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({text, onPress}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <Text style={styles.text}>{text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 8,
+    paddingVertical: 15,
+    paddingHorizontal: 38,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: Fonts.lg,
+    fontWeight: '500',
+    paddingHorizontal: 8,
+    color: Colors.secondary,
+  },
+});
+
+export default Button;
