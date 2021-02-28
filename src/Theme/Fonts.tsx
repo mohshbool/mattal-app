0ac316d5707812d8f88dcf92d1bff10161285990
Mohammad Shbool <alshboolmoh@gmail.com>
2021-02-28 23:42:01 +0200

    theme & fonts

---
+++ src/Theme/Fonts.tsx
@@ -0,0 +1,32 @@
+import {Dimensions, PixelRatio, Platform} from 'react-native';
+
+const {width: SCREEN_WIDTH} = Dimensions.get('window');
+
+// based on iphone 5s's scale
+const scale = SCREEN_WIDTH / 320;
+
+export function normalize(size: number) {
+  const newSize = size * scale;
+  if (Platform.OS === 'ios') {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize));
+  } else {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+  }
+}
+
+export default {
+  regular: 'Montserrat-Regular',
+  bold: 'Montserrat-Bold',
+  extraBold: 'Montserrat-ExtraBold',
+  semiBold: 'Montserrat-SemiBold',
+  xxxsm: normalize(8),
+  xxsm: normalize(10),
+  xsm: normalize(12),
+  sm: normalize(14),
+  md: normalize(16),
+  lg: normalize(18),
+  l: normalize(20),
+  xl: normalize(22),
+  xxl: normalize(24),
+  xxxl: normalize(28),
+};
--- src/Theme/Theme.ts
+++ src/Theme/Theme.ts
@@ -1,5 +1,4 @@
 export const Colors = {
-  //   primary: '#3259ff',
   primary: '#252525',
   secondary: '#fdca6a',
   background: '#c5baae',
