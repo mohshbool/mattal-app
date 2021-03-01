11d61a57ad58fa86e7e8ef2e8f5d93a46ad15a0e
Mohammad Shbool <alshboolmoh@gmail.com>
2021-03-02 00:24:17 +0200

    add dummy data for the viewpager

--- src/Containers/Home.tsx
+++ src/Containers/Home.tsx
@@ -1,10 +1,6 @@
 import ViewPager from '@react-native-community/viewpager';
-import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {useSelector} from 'react-redux';
-import {AreaReducer} from '../Action/types';
-import {RootState} from '../Reducer';
+import {StyleSheet} from 'react-native';
 import Fonts from '../Theme/Fonts';
 import {Colors} from '../Theme/Theme';
 import {Mattal} from '../types';
@@ -14,9 +10,7 @@ import Select from './Select';
 const data: Mattal[] = [
   {
     area: 'Dabouq',
-    images: [
-      'https://cf.bstatic.com/images/hotel/max1024x768/241/241074118.jpg',
-    ],
+    images: ['https://cdn.wallpapersafari.com/69/58/qcUm7T.jpg'],
     name: 'Al Mattal',
     review: 4,
     maps_url: 'https://goo.gl/maps/UvE2K78aksfGqhiQ6',
@@ -25,13 +19,23 @@ const data: Mattal[] = [
       food: false,
     },
   },
+  {
+    area: 'Dabouq',
+    images: [
+      // 'https://lh5.googleusercontent.com/p/AF1QipO-Z0IxnD0Xx6waGhGnx5u6Ag4LdprtBWAS0B59=s901-k-no',
+      'https://www.alessandromichelazzi.com/wp-content/uploads/2020/11/iPhone-12-Pro-Max-Portrait-Mode-Bokeh-Lens-65mm-Alessandro-Michelazzi-05.jpg',
+    ],
+    name: 'Mattal Talia',
+    review: 2,
+    maps_url: 'https://goo.gl/maps/UvE2K78aksfGqhiQ6',
+    facilities: {
+      supermarket: true,
+      food: true,
+    },
+  },
 ];
 
 const Home: React.FC = () => {
-  const {navigate} = useNavigation();
-  const {selectedArea} = useSelector<RootState>(
-    (state) => state.Area,
-  ) as AreaReducer;
   return (
     <ViewPager style={styles.container} initialPage={0} orientation="vertical">
       <Select />
