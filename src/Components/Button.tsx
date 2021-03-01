4c1d4ed574e8c659735e20ca8e7476454114ee44
Mohammad Shbool <alshboolmoh@gmail.com>
2021-03-02 00:23:13 +0200

    outline support

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextStyle,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from 'react-native';
 import Fonts from '../Theme/Fonts';
 import {Colors} from '../Theme/Theme';
 
 interface ButtonProps {
   text: string;
   onPress: () => void;
+  outlined?: boolean;
+  containerStyle?: ViewStyle;
+  textStyle?: TextStyle;
 }
 
-const Button: React.FC<ButtonProps> = ({text, onPress}) => {
+const Button: React.FC<ButtonProps> = ({
+  text,
+  onPress,
+  outlined,
+  containerStyle,
+  textStyle,
+}) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <View style={styles.container}>
-        <Text style={styles.text}>{text}</Text>
-      </View>
+    <TouchableOpacity
+      onPress={onPress}
+      style={[
+        styles.container,
+        containerStyle,
+        outlined ? styles.outlinedContainer : {},
+      ]}>
+      <Text
+        style={[styles.text, textStyle, outlined ? styles.outlinedText : {}]}>
+        {text}
+      </Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    margin: 8,
+    minHeight: 25,
+    minWidth: 40,
     paddingVertical: 15,
     paddingHorizontal: 38,
     borderRadius: 20,
@@ -34,6 +58,14 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     paddingHorizontal: 8,
     color: Colors.secondary,
+  },
+  outlinedContainer: {
+    borderWidth: 2.5,
+    borderColor: Colors.white,
+    backgroundColor: 'transparent',
+  },
+  outlinedText: {
+    color: Colors.white,
   },
 });
 
