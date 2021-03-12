a29a133f0dd9b5b1ebfc8332c8dedae314d69921
Mohammad Shbool <alshboolmoh@gmail.com>
2021-03-13 01:13:37 +0200

    add header

--- src/Containers/Select.tsx
+++ src/Containers/Select.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import * as _ from 'lodash';
-import {SafeAreaView, StyleSheet, View, FlatList} from 'react-native';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useSelector} from 'react-redux';
 
@@ -9,6 +17,8 @@ import AreaCard from '../Components/AreaCard';
 import {Colors} from '../Theme/Theme';
 import {ConfigsReducer} from '../Action/types';
 import {RootState} from '../Reducer';
+import Text from '../Components/Text';
+import Fonts from '../Theme/Fonts';
 
 const Select: React.FC = () => {
   const [areas, setAreas] = React.useState<string[]>([]);
@@ -42,6 +52,32 @@ const Select: React.FC = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
+        <View style={styles.header}>
+          <View>
+            <TouchableOpacity
+              onPress={() =>
+                Linking.openURL('https://www.instagram.com/mattal.app')
+              }>
+              <Ionicon
+                name="ios-logo-instagram"
+                size={Fonts.xxxl}
+                color={Colors.primary}
+              />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text text="Mattal" style={styles.headerText} />
+          </View>
+          <View>
+            <TouchableOpacity onPress={() => console.log('')}>
+              <Ionicon
+                name="ios-information-circle-outline"
+                size={Fonts.xxxl}
+                color={Colors.primary}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
         <FlatList
           data={_.map(areas, (name, index) => ({
             name,
@@ -71,6 +107,18 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: Colors.background,
+  },
+  header: {
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 25,
+    paddingBottom: 5,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+  },
+  headerText: {
+    fontSize: Fonts.xl,
   },
   container: {
     flex: 1,
